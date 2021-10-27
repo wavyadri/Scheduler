b653034f88ce817319e145fcdc292cde1cd5675f
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-10-27 17:29:16 -0400

    create new component with styles and storybook

---
+++ src/components/Appointment/Status.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// PROPS
+// message:String eg. "Deleting"
+
+export default function Status(props) {
+  return (
+    <main className='appointment__card appointment__card--status'>
+      <img
+        className='appointment__status-image'
+        src='images/status.png'
+        alt='Loading'
+      />
+      <h1 className='text--semi-bold'>{props.message}</h1>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf('Button', module)
   .addParameters({
@@ -137,4 +141,12 @@ storiesOf('Appointment', module)
   .add('Appointment', () => <Appointment />)
   .add('Appointment with Time', () => <Appointment time='12pm' />)
   .add('Header', () => <Header time='12pm' />)
-  .add('Empty', () => <Empty onAdd={action('onAdd')} />);
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show onEdit={action('onEdit')} onDelete={action('onDelete')} />
+  ))
+  .add('Confirm', () => (
+    <Confirm onCancel={action('onCancel')} onConfirm={action('onConfirm')} />
+  ))
+  .add('Status', () => <Status message={'Deleting...'} />)
+  .add('Error', () => <Error onClose={action('onClose')} />);
