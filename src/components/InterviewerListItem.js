61b4499076561c9b0ab6ea9f93d274a963657fe7
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-10-27 14:12:05 -0400

    build component with styles and storybook

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'components/InterviewerListItem.scss';
+import classnames from 'classnames';
+
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - determines if an interviewer is selected or not and displays the name and applies appropriate styles if selected.
+// setInterviewer:function - is run when the InterviewerListItem is clicked. This function receives the interviewer's id as an argument. It sets the selected interviewer.
+
+export default function InterviewerListItem(props) {
+  const listItemClass = classnames('interviewers__item', {
+    'interviewers__item--selected': props.selected,
+  });
+
+  return (
+    <li className={listItemClass} onClick={props.setInterviewer}>
+      <img
+        className='interviewers__item-image'
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
