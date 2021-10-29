c959d1c05dae132653905d55066e8e747a148909
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-10-29 16:50:56 -0400

    implement useVisualMode to render show, empty and form components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,24 +2,49 @@ import React from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
 import 'components/Appointment/styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
 
 // PROPS
 // id
 // time
 // interview
 
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className='appointment'>
       <Header time={props.time} />
-      {props.interview ? (
+      {/* {props.interview ? (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       ) : (
         <Empty />
+      )} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          // interviewers={props.interviewers}
+          interviewers={[]}
+          onCancel={back}
+          // onSave={onSave}
+        />
       )}
     </article>
   );
