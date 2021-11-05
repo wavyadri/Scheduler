677898663a1f1bcc2d8457738b824804890f4613
Adriana Calvo-Matos <adriana.calvomatos@gmail.com>
2021-11-04 20:40:58 -0400

    refactor form test

--- src/components/__tests__/ Form.test.js
+++ src/components/__tests__/ Form.test.js
@@ -46,9 +46,11 @@ describe('Form', () => {
 
   it('can successfully save after trying to submit an empty student name', () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
+
+    fireEvent.click(getByAltText('Sylvia Palmer'));
 
     fireEvent.click(getByText('Save'));
 
@@ -64,7 +66,7 @@ describe('Form', () => {
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', null);
+    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', interviewers[0].id);
   });
 
   it('calls onCancel and resets the input field', () => {
